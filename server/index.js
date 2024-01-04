93725d5027501820a856572947a7df6d383ed266
Hanzala-Shakeel <102066085+Hanzala-Shakeel@users.noreply.github.com>
2024-01-05 00:18:34 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require("cors");
-const taskModel = require("./mongodb");
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const taskModel = require('./mongodb');
 
 app.use(express.json());
 
@@ -12,37 +12,43 @@ app.use(cors({
 }));
 
 app.get('/', function (req, res) {
-    res.send("<h1>Home Page</h1>")
-})
+    res.send("<h1>Home Page</h1>");
+});
 
 app.post('/add', async function (req, res) {
     const todo = req.body.todo;
-    let createdtask = await taskModel.create({
-        task: todo,
-        isDone: false
-    })
-        .then(res => res.json(createdtask))
-        .catch(err => res.json(err));
+    try {
+        const createdTask = await taskModel.create({
+            task: todo,
+            isDone: false
+        });
+        res.json(createdTask);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
-app.get("/get", async function (req, res) {
-    let allTasks = await taskModel.find()
-    res.json(allTasks)
-    // .then(res=>res.send(allTasks))
-    // .catch(err=>res.send(err));
+app.get('/get', async function (req, res) {
+    try {
+        let allTasks = await taskModel.find();
+        res.json(allTasks);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
-app.delete("/delete", async function (req, res) {
+app.delete('/delete', async function (req, res) {
     const deleteId = req.body.IdForDelete;
-    console.log(deleteId);
-    let deleteTask = await taskModel.findOneAndDelete({ _id: deleteId })
-        .then(res => res.json(deleteTask))
-        .catch(err => res.json(err));
+    try {
+        let deleteTask = await taskModel.findOneAndDelete({ _id: deleteId });
+        res.json(deleteTask);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
-app.put("/update", async function (req, res) {
+app.put('/update', async function (req, res) {
     const { _id, isChecked } = req.body;
-    console.log(_id);
     try {
         const updatedTask = await taskModel.findByIdAndUpdate(
             _id,
@@ -55,5 +61,5 @@ app.put("/update", async function (req, res) {
     }
 });
 
-
 app.listen(3001);
+);
