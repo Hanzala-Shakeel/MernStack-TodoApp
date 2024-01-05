eb78d3844e473ba23143c0b7705723b149ea49bf
Hanzala-Shakeel <102066085+Hanzala-Shakeel@users.noreply.github.com>
2024-01-05 19:42:27 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,13 +3,11 @@ const app = express();
 const cors = require('cors');
 const taskModel = require('./mongodb');
 
-app.use(cors(
-    {
-        origin: ["https://mern-stack-todo-app-frontend.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+app.use(cors({
+  origin: 'https://mern-stack-todo-app-frontend.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}));
 
 app.use(express.json());
 
