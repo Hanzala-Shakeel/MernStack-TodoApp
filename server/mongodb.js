42507001a97b8a0110970f456b5ae624c9161952
Hanzala-Shakeel <102066085+Hanzala-Shakeel@users.noreply.github.com>
2024-12-22 18:13:14 +0500

    Update mongodb.js

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // mongoose.connect("mongodb://127.0.0.1:27017/mytodo");
-mongoose.connect("mongodb+srv://rumaisa:123@cluster0.48vpiei.mongodb.net/todoData?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://rumaisa:123@cluster0.hvu5y.mongodb.net/todoData?retryWrites=true&w=majority");
 
 let tasksSchema = mongoose.Schema({
   task: String,
